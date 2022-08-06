36052eb010ea5fdb3d50806817c75d2cff458d5e
nikk8721 <nikk8721@gmail.com>
2022-08-07 00:35:03 +0500

    Beginning of the main program, intermediate test

---
+++ Task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Сформировать из имеющего массива массив из строк,
+// длина которых меньше или равна 3.
+Console.WriteLine("Введите кол-во элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] Arr = new string [n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите значения: ");
+    Arr[i] = (Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", Arr) + "]");
+// Формирование массива по условию
