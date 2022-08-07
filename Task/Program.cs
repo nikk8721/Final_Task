c791488f1ee40ffa69af386000d1a77f57c08701
nikk8721 <nikk8721@gmail.com>
2022-08-07 14:19:42 +0500

    Program test for execution, successful

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,6 +3,7 @@
 Console.WriteLine("Введите кол-во элементов массива:");
 int n = Convert.ToInt32(Console.ReadLine());
 string[] Arr = new string [n];
+string[] Arr2 = new string [n];
 for (int i = 0; i < n; i++)
 {
     Console.Write("Введите значения: ");
@@ -11,3 +12,20 @@ for (int i = 0; i < n; i++)
 Console.WriteLine();
 Console.WriteLine("Начальный массив: [" + string.Join(", ", Arr) + "]");
 // Формирование массива по условию
+for (int i = 0; i < n; i++)
+{
+    string s = Arr[i];
+    if (s.Length <= 3)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Arr2[i] = s;
+        }
+        string temp;
+        temp = Arr2[i];
+        Arr2[i] = Arr [i];
+        Arr[i] = temp;
+    }
+    
+}
+Console.WriteLine("Итоговый массив: [" + string.Join(", ", Arr2) + "]");                    
